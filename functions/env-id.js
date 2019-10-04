8b45de5c25b730c3140ae5667d341a0be95c25e0
ayumitk <ayumee528@gmail.com>
2019-10-04 08:37:30 -0700

    delete functions

--- functions/env-id.js
+++
@@ -1,6 +0,0 @@
-const { GATSBY_ALGOLIA_APP_ID } = process.env
-
-exports.handler = async (event, context) => ({
-  statusCode: 200,
-  body: GATSBY_ALGOLIA_APP_ID,
-})
--- functions/env-key.js
+++
@@ -1,6 +0,0 @@
-const { GATSBY_ALGOLIA_ADMIN_API_KEY } = process.env
-
-exports.handler = async (event, context) => ({
-  statusCode: 200,
-  body: GATSBY_ALGOLIA_ADMIN_API_KEY,
-})
