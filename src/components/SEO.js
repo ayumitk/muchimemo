53e9ac37a42b25541a49a559aebb1ecd9054347d
ayumitk <ayumee528@gmail.com>
2020-08-18 21:56:17 -0700

    remove google analytics tag

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -177,15 +177,6 @@ const SEO = props => {
       <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script>
 
       <meta name="google-site-verification" content="ugXDabOeIzHktqQo6ZhCRdh4EZWaMVO5-qhndwhLGDI" />
-      {/* Global site tag (gtag.js) - Google Analytics */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121956784-2"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-121956784-2');
-      </script>
-
     </Helmet>
   )
 }
