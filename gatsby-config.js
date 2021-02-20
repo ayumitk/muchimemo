7805aa95f1abdd84865fa00332143bd095b2a485
ayumitk <ayumee528@gmail.com>
2021-02-19 16:25:49 -0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ const cfg = {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/static`,
         name: 'uploads',
       },
     },
